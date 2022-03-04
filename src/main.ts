e5baa66ffc2730ef35dec3c02a1f9b73edca5c03
yasha <ysahakyan1998@gmail.com>
2022-03-04 12:22:08 +0400

    test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ComputerModule);
+  console.log('hello');
   await app.listen(3000);
 }
 
